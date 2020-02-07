3097cda5c5821c1f5e89d8885536b74ad46ad56e
Primode <60574893+Primode@users.noreply.github.com>
2020-02-07 23:06:46 +0100

    Delete nav.js

--- nav.js
+++
@@ -1,8 +0,0 @@
-        function update() {
-            if ($(window).scrollTop() > 1) {
-    $('nav').attr('class', 'nav-active');
-  } else {
-    $('.nav-active').attr('class', 'nav');
-  }
-}
-setInterval(update, 0);
