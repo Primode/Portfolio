bf41ed8935940acef4c411facba8d6e1e1f33b3b
Primode <60574893+Primode@users.noreply.github.com>
2020-02-07 23:06:33 +0100

    Delete scroll.js

--- scroll.js
+++
@@ -1,6 +0,0 @@
-$('a').click(function(){
-    var $href = $(this).attr('href');
-    var $anchor = $($href).offset().top - 63;
-    $('html, body').animate({ scrollTop: $anchor }, 1000);
-    return false;
-});
